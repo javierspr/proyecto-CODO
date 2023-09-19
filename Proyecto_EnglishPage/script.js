cf309f0a3761aead76c8206c22c63ec229ddf54d
quivo2927 <nat.aramendi@hotmail.comgit config --global user.name quivo2927git config --global user.email nat.aramendi@hotmail.com>
2023-09-19 18:06:38 -0300

    agregado paleta de colores y footer falta header

---
+++ Proyecto_EnglishPage/script.js
@@ -0,0 +1,37 @@
+console.log("empezando..");
+console.log(4+5)
+document.write("<h2 style=´color:red´>  texto incorpardo por js</h2>"); /*lo que se 
+escribe en js va primero en la pagina */
+console.log("voy a cumplir", 45+1, "anios el 25/04")
+console.info("mensaje de informacion")
+console.warn("texto de alerta")
+console.error("texto de error")
+console.clear("limpia la consola")
+//variables
+var variable
+var variable2=2
+console.log(variable) //indefined
+console.log(variable2) //imprime 2
+
+var linea="........................."
+console.log(linea)
+var dato=37
+console.log(dato)
+dato="jose"
+console.log(daato)
+dato=true
+
+//constantes
+console.log(linea)
+const IVA=21
+console.log(IVA)
+IVA=10
+console.log(IVA)
+
+//typeof tipo de dato
+console.log(linea)
+var dato2
+dato2=2
+
+console.log(typeof dato2) //muestra el tipo de dato
+
