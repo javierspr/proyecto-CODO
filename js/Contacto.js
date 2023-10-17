3ecc458136c4d5fc038dc2d13d6a8c45da7ee5db
Celina Etienot <celinaetienot@gmail.com>
2023-10-16 22:27:35 -0300

    modifiaciones validacion formulario contacto

--- js/Contacto.js
+++ js/Contacto.js
@@ -1,15 +1,22 @@
-  document.addEventListener("DOMContentLoaded", function () {
-    const form = document.querySelector("form");
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.querySelector("form");
 
-    form.addEventListener("submit", function (event) {
-      const nombre = document.getElementById("nombre").value.trim();
-      const email = document.getElementById("email").value.trim();
-      const mensaje = document.getElementById("mensaje").value.trim();
+  form.addEventListener("submit", function (event) {
+    const nombre = document.getElementById("nombre").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const mensaje = document.getElementById("mensaje").value.trim();
+  
+    // Validación de campos vacíos
+    if (nombre === "" || email === "" || mensaje === "") {
+      event.preventDefault(); // Prevenir el envío del formulario
+      alert("Por favor, complete todos los campos del formulario.");
+    }
 
-      if (nombre === "" || email === "" || mensaje === "") {
-        event.preventDefault(); // Prevent form submission
-        alert("Por favor, complete todos los campos del formulario.");
-      }
-    });
+    // Validación de formato de correo electrónico
+    const emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
+    if (!email.match(emailRegex)) {
+      event.preventDefault(); // Prevenir el envío del formulario
+      alert("Formato incorrecto. Ingrese un correo electrónico válido.");
+    }
   });
-
+});
