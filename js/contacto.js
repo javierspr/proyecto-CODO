7b7a0698f6d647c8a790afe9ab9189d297275079
Celina Etienot <celinaetienot@gmail.com>
2023-10-25 12:16:01 -0300

    ..

---
+++ js/contacto.js
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.querySelector("form");
+
+  form.addEventListener("submit", function (event) {
+    const nombre = document.getElementById("nombre").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const mensaje = document.getElementById("mensaje").value.trim();
+
+    // Validación de campos vacíos
+    if (nombre == 0 || email == 0 || mensaje == 0) {
+      event.preventDefault(); // Prevenir el envío del formulario
+      alert("Por favor, complete todos los campos del formulario.");
+    }
+
+    // Validación de formato de correo electrónico
+    const emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
+    if (!email.match(emailRegex)) {
+      event.preventDefault(); // Prevenir el envío del formulario
+      alert("Formato incorrecto. Ingrese un correo electrónico válido.");
+    }
+  });
+});
+
+
+var preguntas = document.querySelectorAll(".pregunta");
+
+preguntas.forEach(function (pregunta) {
+  pregunta.addEventListener("click", function () {
+    var respuesta = pregunta.nextElementSibling;
+    if (respuesta.style.display === "none" || respuesta.style.display === "") {
+      respuesta.style.display = "block";
+    } else {
+      respuesta.style.display = "none";
+    }
+  });
+});
